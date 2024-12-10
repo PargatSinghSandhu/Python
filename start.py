5b052824b0248ca272514163c6324fa17843bf08
PargatSinghSandhu <sandhu.pargat07@gmail.com>
2024-12-10 17:46:50 -0500

    Py

---
+++ start.py
@@ -0,0 +1,15 @@
+def get_choice():
+    player_choice = "rock"
+    computer_choice = "paper"
+    
+    return computer_choice
+
+def greeting():
+    return "Hi"
+
+def optimize():
+    dict = {"one": "Akon", "two" : "Justin", "three": "Pargat"}
+    print(dict["one"])
+    print(dict["two"])
+    
+optimize()
